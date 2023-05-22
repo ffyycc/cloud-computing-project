7d43036af87c5ecfdcf8385e5ff264a480c7c843
Xin Shu <xinshu2023@u.northwestern.edu>
2023-05-22 13:53:57 -0500

    update the model evaluation part

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -39,19 +39,21 @@ def plot_results(model_results: pd.DataFrame) -> dict:
     fig_dict = {}
     try:
         # Histogram
-        fig1 = model_results.hist(figsize=(20, 8))
+        fig1, ax1 = plt.subplots(figsize=(20, 8))
+        model_results.hist(ax=ax1)
         fig_dict["histogram"] = fig1
 
         # Density plot
-        fig2 = model_results.plot(kind='kde', figsize=(20, 8))
+        fig2, ax2 = plt.subplots(figsize=(20, 8))
+        model_results.plot(kind='kde', ax=ax2)
         fig_dict["density_plot"] = fig2
 
         # Scatterplot of actual price vs predicted price   
-        fig3 = plt.figure(figsize=(20, 8)) 
-        plt.scatter(model_results['Actual Price'], model_results['Predicted Price'], color='green')
-        plt.xlabel('Actual Price')
-        plt.ylabel('Predicted Price')
-        plt.title('Actual Price vs Predicted Price')    
+        fig3, ax3 = plt.subplots(figsize=(20, 8)) 
+        ax3.scatter(model_results['Actual Price'], model_results['Predicted Price'], color='green')
+        ax3.set_xlabel('Actual Price')
+        ax3.set_ylabel('Predicted Price')
+        ax3.set_title('Actual Price vs Predicted Price')    
         fig_dict["scatterplot"] = fig3
 
         logging.info("Plots created successfully.")
@@ -59,7 +61,6 @@ def plot_results(model_results: pd.DataFrame) -> dict:
     except Exception as e:
         logging.error("Error occurred while generating plots: " + str(e))
         return None
-
 
 def save_graphs(fig_dict: dict, plot_dir: Path) -> None:
     """
@@ -73,10 +74,16 @@ def save_graphs(fig_dict: dict, plot_dir: Path) -> None:
         None
     """
     try:
+        # Check if the directory exists, if not, create it
+        plot_dir.mkdir(parents=True, exist_ok=True)
+
         for fig_name, fig in fig_dict.items():
-            fig.savefig(plot_dir / f'{fig_name}.png')
-            plt.close(fig)
-        
+            if isinstance(fig, plt.Figure):  # ensure the object is a Matplotlib figure
+                fig.savefig(plot_dir / f'{fig_name}.png')
+                plt.close(fig)
+            else:
+                logging.error(f"Object under key '{fig_name}' is not a Matplotlib figure.")
+
         logging.info("Graphs saved successfully.")
     except Exception as e:
         logging.error("Error occurred while saving graphs: " + str(e))
