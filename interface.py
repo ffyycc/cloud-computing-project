2f0ad21e546580ccc35003d96d5ba0bc5aeb188e
Bonnie Ao <bonnie.ao@outlook.com>
2023-05-28 11:00:23 -0500

    editinterface

--- interface.py
+++ interface.py
@@ -2,20 +2,13 @@ import os
 import re
 from pathlib import Path
 import logging
-import datetime
-
+from typing import Dict
 import botocore
 import joblib
 import yaml
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-
+import streamlit as st
 import src.aws_utils as aws
 import src.present_interface as pi
-
-import streamlit as st
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -26,8 +19,16 @@ BUCKET_NAME = os.getenv("BUCKET_NAME", "cloud-project-models")
 # ARTIFACTS_PREFIX = os.getenv("ARTIFACTS_PREFIX", "artifacts/")
 CONFIG_REF = os.getenv("CONFIG_REF", "config/config.yml")
 
+def load_config(config_ref: str) -> Dict:
+    """
+    Load the configuration file from local path or S3.
 
-def load_config(config_ref: str) -> dict:
+    Parameters:
+        config_ref (str): Reference to the configuration file. This can be a local path or a S3 URI.
+
+    Returns:
+        dict: The loaded configuration as a dictionary.
+    """
     if config_ref.startswith("s3://"):
         # Get config file from S3
         config_file = Path("config/downloaded-config.yaml")
@@ -50,45 +51,59 @@ def load_config(config_ref: str) -> dict:
         return yaml.load(f, Loader=yaml.SafeLoader)
 
 
-def main():
+def main() -> None:
+    """
+    Main function to run the Streamlit app.
+
+    This function loads the configuration, sets up the output directory, 
+    loads the preprocessor and model, and presents the user interface.
+    """
+    logger.info("Loading configuration...")
     config = load_config(CONFIG_REF)
     run_config = config.get("run_config", {})
 
     # Set up output directory for saving artifacts
-    artifacts = Path(run_config.get("output", "runs"))
-    
+    artifacts_out = Path(run_config.get("output", "runs"))
     processor_s3_key = config["aws"]["selected_preprocessor_key"]
-    
     @st.cache_resource
-    def load_preprocessor():
-        aws.download_s3(BUCKET_NAME, processor_s3_key, artifacts / processor_s3_key)
-        preprocessor = joblib.load(artifacts / processor_s3_key)
-        return preprocessor
-        
-    preprocessor = load_preprocessor()
-    
-    @st.cache_resource
-    def load_model(cur_model_s3_key):
-        print("loading artifacts from: ", artifacts.absolute())
-        # Download model and preprocessor from S3
-        aws.download_s3(BUCKET_NAME, cur_model_s3_key, artifacts / cur_model_s3_key)
-     
-        # Load model from the downloaded file
-        model = joblib.load(artifacts / model_s3_key)
-      
-        return model
- 
-    st.title("We Can Make House Price Prediction in Perth for You!")
+    def load_preprocessor() -> object:
+        """
+        Loads the preprocessor from S3.
 
+        Returns:
+            object: The preprocessor object.
+        """
+        logger.info("Loading preprocessor from S3...")
+        aws.download_s3(BUCKET_NAME, processor_s3_key, artifacts_out / processor_s3_key)
+        preprocessor = joblib.load(artifacts_out / processor_s3_key)
+        return preprocessor
+    preprocessor = load_preprocessor()
+    @st.cache_resource
+    def load_model(cur_model_s3_key: str) -> object:
+        """
+        Loads the model from S3.
+
+        Parameters:
+            cur_model_s3_key (str): S3 key for the model to be loaded.
+
+        Returns:
+            object: The model object.
+        """
+        logger.info("Loading model from: %s...", artifacts_out.absolute())
+        # Download model and preprocessor from S3
+        aws.download_s3(BUCKET_NAME, cur_model_s3_key, artifacts_out / cur_model_s3_key)
+        # Load model from the downloaded file
+        model = joblib.load(artifacts_out / model_s3_key)
+        return model
+    logger.info("Setting up Streamlit title and sidebar...")
+    st.title("We Can Make House Price Prediction in Perth for You!")
     st.sidebar.header("User Input Parameters")
- 
     # Sidebar to choose model
     # Set up the sidebar
     model_choice = st.sidebar.selectbox(
         "Choose the model",
         ("XGBoost", "Random Forest", "Logistic Regression")
     )
-
     # Depending on the choice, instantiate the correct model
     if model_choice == "XGBoost":
         model_s3_key = config["aws"]["xgboost_model_name"]
@@ -96,11 +111,9 @@ def main():
         model_s3_key = config["aws"]["random_forest_model_name"]
     elif model_choice == "Logistic Regression":
         model_s3_key = config["aws"]["logistic_regression_model_name"]
-    
     model= load_model(model_s3_key)
-   
-
     # Present user interface
+    logger.info("Presenting user interface...")
     pi.present_interface(model,preprocessor)
 
 if __name__ == "__main__":
